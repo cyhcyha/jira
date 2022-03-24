4b0bfcb03d62daba5483b8856becf4fbd42a79a8
cyh <20334816@qq.com>
2022-03-24 16:26:32 +0800

    修改之前的代码，将js改为ts形式

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
-export const isFalsy = (value) => (value === 0 ? true : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? true : !value);
 
 //在一个函数里，改变传入的对象是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: { [key: string]: unknown }) => {
   const result = { ...object };
   Object.keys(object).forEach((key) => {
     const value = object[key];
