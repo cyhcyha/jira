f9a275a755c51413ee555e240ee71611f9af54d1
cyh <20334816@qq.com>
2022-03-25 15:41:36 +0800

    实现useMount和useDebounce 添加types和assets 添加jira-devtool，添加craco，添加antd，添加emotion

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value: unknown) => (value === 0 ? true : !value);
 
 //在一个函数里，改变传入的对象是不好的
@@ -10,4 +12,25 @@ export const cleanObject = (object: { [key: string]: unknown }) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = <V>(value: V, delay?: number) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    //每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+    //每次在上一个useEffect处理完以后运行
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+  return debounceValue;
 };
